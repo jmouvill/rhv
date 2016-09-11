2cdc979770e43826d7715eb69c19cdb04df5d315
Mouvilliat <Jean@MacBook-Pro-de-Mouvilliat.local>
2016-09-11 18:46:10 +0200

    some features engineering

--- rhv.R
+++ rhv.R
@@ -13,16 +13,7 @@ train <- data.frame(train)
 train$outcome <- factor(train$outcome)
 test <- fread("/Users/Jean/Documents/workspace/rhv/act_test.csv")
 test <- data.frame(test)
-test %>% names()
-test$outcome <- factor(test$outcome)
-
 summ <- fread("/Users/Jean/Documents/workspace/rhv/sample_submission.csv")
-summ %>% head()
-
-train %>% head()
-train %>% summary()
-
-numberDistinctPeople <- nrow(distinct(train[,c("people_id","people_id")]))
 
 ##data cleaning for training set
 t <- grepl("char_",names(train))
@@ -55,24 +46,41 @@ finalTrain <- merge(x = train, y = people, by = "people_id", all.x = TRUE)
 ##distribution of char 38
 ggplot(finalTrain, aes(x=char_38)) + geom_density(aes(color=outcome))
 
-
-finalTrain %>% head
 finalTrain$eventDate <- as.Date(finalTrain$date.x, format = "%Y-%m-%d")
 trainOrdered <- finalTrain[with(finalTrain, order(people_id,eventDate )),]
 
 #compute the previous average
-trainOrdered %>% head
 trainOrdered$numOutcome <- as.numeric(as.character(trainOrdered$outcome))
 dt <- data.table(trainOrdered)
-dt[,ave:=c(-1,cummean(numOutcome)),by=people_id]
+dt[,historicalOutcome:=c(-1,cummean(numOutcome)),by=people_id]
 trainOrdered <- data.frame(dt)
-trainOrdered %>% subset(select = c(outcome, historicalOutcome)) %>% head(100)
+ggplot(trainOrdered, aes(x =historicalOutcome)) + geom_density(aes(color=outcome))
+trainOrdered$facHistoricalOutcome = ifelse(
+  (trainOrdered$historicalOutcome == 1 |
+    trainOrdered$historicalOutcome == -1 |
+    trainOrdered$historicalOutcome == 0), trainOrdered$historicalOutcome, 2)
+ggplot(trainOrdered, aes(x =facHistoricalOutcome)) + geom_bar() + facet_grid(outcome~.)
+
+##Compute the diff date between inscription and the action
+trainOrdered$diffDate <- as.Date(finalTrain$date.x, format = "%Y-%m-%d") - as.Date(finalTrain$date.y, format = "%Y-%m-%d")
+trainOrdered$diffDate <- as.numeric(trainOrdered$diffDate)
+trainOrdered %>% subset(select = c(date.x,date.y, diffDate)) %>% head 
+ggplot(trainOrdered, aes(x=diffDate)) + geom_density(aes(color=outcome))
 
 ##confused people 
 confused <- trainOrdered[trainOrdered$historicalOutcome != 0 &trainOrdered$historicalOutcome != 1,] 
 ggplot(confused, aes(x =historicalOutcome)) + geom_density(aes(color=outcome))
+ggplot(confused, aes(x =diffDate)) + geom_density(aes(color=outcome))
+ggplot(confused, aes(x =historicalOutcome, y = diffDate)) + geom_point(aes(color=outcome))
+
+summary(trainOrdered)
+
+ggplot(confused, aes(x =char_2.y)) + geom_bar(aes(color=outcome,fill=outcome))
 
 confused %>% subset(select = c(outcome,historicalOutcome,char_38)) %>% summary()
+
+
+
 
 
 
